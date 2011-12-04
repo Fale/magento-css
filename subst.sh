101d5d43637a1d619984599a60c94f137b5a4acf
Fabio Alessandro Locati <f.locati@snds.co>
2011-12-04 17:27:38 +0100

    Fixing the script

--- subst.sh
+++ subst.sh
@@ -7,16 +7,17 @@
 #
 # F.Cermelli                                 snds - 2 dec 2011
 # -------------------------------------------------------------
-PATH_ROOT="~/public_html/skin/frontend/default"
+PATH_ROOT="${HOME}/public_html/skin/frontend/default"
 #PATH_ROOT="${HOME}/magento-css" # DEVELOP
+PATH_HOME="${HOME}/magento-css" # DEVELOP
+PATH_DEF="${PATH_HOME}/default/css"
 
-PATH_DEF="${HOME}/magento-css/default/css"
-FILE_INI="styles.ini"
-FILE_CSS="styles.css"
+INI="styles.ini"
+CSS="styles.css"
 
 
-FILE_DEF="${PATH_DEF}/${FILE_CSS}"
-FILE_INI="${PATH_DEF}/${FILE_INI}"
+FILE_DEF="${PATH_DEF}/${CSS}"
+FILE_INI="${PATH_DEF}/${INI}"
 
 FILE_TMP="/tmp/temporary.tmp"
 FILE_TMP1="/tmp/template1.tmp"
@@ -27,16 +28,15 @@ FILE_TMP2="/tmp/template2.tmp"
 function SetFileSite
 {
   PATH_OUT="${PATH_ROOT}/${SITE_NAME}/css"
-  PATH_SINI="${HOME}/magento-css/${SITE_NAME}/css"
+  FILE_SOUT="${PATH_OUT}/${CSS}"
+#  if [[ ! -r  ${FILE_SOUT} ]]
+#  then
+#    "File Not Found: ${FILE_SOUT}"
+#    exit 2
+#  fi
 
-  FILE_SOUT="${PATH_OUT}/${FILE_CSS}"
-  if [[ ! -r  ${FILE_SOUT} ]]
-  then
-    "File Not Found: ${FILE_SOUT}"
-    exit 2
-  fi
-
-  FILE_SINI="${PATH_INI}/${FILE_INI}"
+  PATH_SINI="${PATH_HOME}/${SITE_NAME}/css"
+  FILE_SINI="${PATH_SINI}/${INI}"
   if [[ ! -r  ${FILE_SINI} ]]
   then
     "File Not Found: ${FILE_SINI}"
@@ -60,7 +60,7 @@ function LoopFileDef
     else
       echo "${INI_VAL}" >> ${FILE_TMP}
     fi
-  done < ${FILE_DEF}
+  done < ${FILE_INI}
 }
 
 
@@ -86,11 +86,14 @@ function Substitute
   (( index = 0 ))
   for i in ${arr_id[*]}
   do
-    sed -e 's/${arr_id[$index]}/${arr_va[$index]}/g' ${FILE_TMP1} > ${FILE_TMP2}
-    mv ${FILE_TMP2} ${FILE_TMP1}
+    sed -e s/${arr_id[$index]}/${arr_va[$index]}/g ${FILE_TMP1} > ${FILE_TMP2}
+   echo "    sed -e 's/${arr_id[$index]}/${arr_va[$index]}/g' ${FILE_TMP1} > ${FILE_TMP2}"
+#   diff ${FILE_TMP2} ${FILE_TMP1}
+ 
+    cp ${FILE_TMP2} ${FILE_TMP1}
     (( index = index + 1 ))
   done
-  mv ${FILE_TMP1} ${FILE_SOUT}
+  cp ${FILE_TMP1} ${FILE_SOUT}
 }
 
 
