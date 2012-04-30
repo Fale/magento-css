0d2fb0e39cb6cf88bf239ffb75d03ad447215f3c
Fabio Alessandro Locati <f.locati@snds.co>
2012-04-30 12:14:14 +0200

    Add another file to the parser

--- subst.sh
+++ subst.sh
@@ -12,11 +12,13 @@ PATH_ROOT="${HOME}/public_html/skin/frontend/snds"
 PATH_HOME="${HOME}/magento-css"
 PATH_DEF="${PATH_HOME}/templates/default/css"
 
+SCSS="styles.css"
+WCSS="widgets.css"
+
+FILE_DEF[0]="${PATH_DEF}/${SCSS}"
+FILE_DEF[1]="${PATH_DEF}/${WCSS}"
+
 INI="styles.ini"
-CSS="styles.css"
-
-
-FILE_DEF="${PATH_DEF}/${CSS}"
 FILE_INI="${PATH_DEF}/${INI}"
 
 FILE_TMP="/tmp/temporary.tmp"
@@ -28,7 +30,8 @@ FILE_TMP2="/tmp/template2.tmp"
 function SetFileSite
 {
   PATH_OUT="${PATH_ROOT}/${SITE_NAME}/css"
-  FILE_SOUT="${PATH_OUT}/${CSS}"
+  FILE_SOUT[0]="${PATH_OUT}/${SCSS}"
+  FILE_SOUT[1]="${PATH_OUT}/${WCSS}"
   PATH_SINI="${PATH_HOME}/${SITE_NAME}/css"
   FILE_SINI="${PATH_SINI}/${INI}"
   if [[ ! -r  ${FILE_SINI} ]]
@@ -75,16 +78,22 @@ function BuildArray
 # ----------------------
 function Substitute
 {
-  cp -f ${FILE_DEF} ${FILE_TMP1}
-  chmod 666 ${FILE_TMP1}
-  (( index = 0 ))
-  for i in ${arr_id[*]}
+  CSS_FILE=${#FILE_SOUT[*]}
+  (( css_index = 0 ))
+  while (( css_index < ${CSS_FILE} ))
   do
-    sed -e s/${arr_id[$index]}/${arr_va[$index]}/g ${FILE_TMP1} > ${FILE_TMP2}
-    cp ${FILE_TMP2} ${FILE_TMP1}
-    (( index = index + 1 ))
+    cp -f ${FILE_DEF[css_index]} ${FILE_TMP1}
+    chmod 666 ${FILE_TMP1}
+    (( index = 0 ))
+    for i in ${arr_id[*]}
+    do
+      sed -e s/${arr_id[$index]}/${arr_va[$index]}/g ${FILE_TMP1} > ${FILE_TMP2}
+      cp ${FILE_TMP2} ${FILE_TMP1}
+      (( index = index + 1 ))
+    done
+    cp ${FILE_TMP1} ${FILE_SOUT[$css_index]}
+    (( css_index = css_index +1 ))
   done
-  cp ${FILE_TMP1} ${FILE_SOUT}
 }
 
 
